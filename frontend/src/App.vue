04386689c3e8a0544190b2e51795991e3270869d
admin <admin@admins-MacBook-Pro.local>
2025-11-27 22:20:04 +0700

    Initial Squadtix project structure (backend + frontend)

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const testApi = async () => {
+  const res = await fetch('/api/hello');
+  const data = await res.json();
+  console.log(data);
+};
+
+testApi();
+</script>
+
+<template>
+  <h1>Squadtix Frontend Connected!</h1>
+</template>
