8009ae2719e1f69722c559f5c9e2b295d6f66be7
konsolnasbawaslu <konsolnasbawaslu@srv1154977.hstgr.cloud>
2025-12-05 01:06:45 +0000

    terbaru dari server

--- frontend/src/components/layout/header/HeaderLogo.vue
+++ frontend/src/components/layout/header/HeaderLogo.vue
@@ -1,9 +1,9 @@
 <template>
-  <router-link to="/" class="lg:hidden">
-    <img class="dark:hidden" src="/images/logo/logo.svg" alt="Logo" />
-    <img class="hidden dark:block" src="/images/logo/logo-dark.svg" alt="Logo" />
+  <router-link to="/" class="lg:hidden inline-block">
+    <span class="invisible">.</span>
   </router-link>
 </template>
+
 
 <script setup>
 import { RouterLink } from 'vue-router'
